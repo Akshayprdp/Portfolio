4a4f63fe0761fd0382091d745b08b491ad58f5d1
Akshayprdp <Akshayprdp21@gmail.com>
2024-07-13 16:41:39 +0530

    Design

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,28 +1,58 @@
-
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div className='About-div'>
-  
-    <div className='Aboutdiv1'>
-      <h1>Akshay P</h1>
-      <h2>Full stack Developer</h2>
+    <div className="About-div">
+      <div className="Aboutdiv1">
+        <h1>Akshay P</h1>
+        <h2>Full stack Developer</h2>
       </div>
-      
-      <div className='Aboutdiv2'>
-        <h1 className='Aboutdiv3'>
-            About -
-        </h1>
-      <h1>
-        Hi,i'm Akshay
-      </h1>
-      <p className='Aboutdiv4'>
-                I'm Akshay, a MERN stack Developer currently interning at Softroniics Perindalmanna. In this role, I am gaining extensive hands-on experience in building full-stack web applications using MongoDB, Express.js, React.js, and Node.js. My work involves crafting responsive user interfaces, developing robust server-side components, and implementing seamless frontend-backend communication.
-      </p>
+
+      <div className="Aboutdiv2">
+        <h1 className="Aboutdiv3">About -</h1>
+        <h1>Hi, I'm Akshay</h1>
+        <p className="Aboutdiv4">
+          I'm Akshay, a MERN stack Developer currently interning at Softroniics
+          Perindalmanna. In this role, I am gaining extensive hands-on
+          experience in building full-stack web applications using MongoDB,
+          Express.js, React.js, and Node.js. My work involves crafting
+          responsive user interfaces, developing robust server-side components,
+          and implementing seamless frontend-backend communication.
+        </p>
       </div>
-   
+      <div className="shapes">
+        <div className="rectangle1"></div>
+        <div className="rectangle2"></div>
+        <div className="rectangle3"></div>
+        <div className="rectangle4"></div>
+        <div className="rectangle5"></div>
+        <div className="rectangle6"></div>
+        <div className="rectangle7"></div>
+        <div className="rectangle8"></div>
+        <div className="rectangle9"></div>
+        <div className="rectangle10"></div>
+        <div className="rectangle11"></div>
+        <div className="rectangle12"></div>
+        <div className="rectangle13"></div>
+        <div className="rectangle14"></div>
+        <div className="rectangle15"></div>
+        <div className="rectangle16"></div>
+        <div className="rectangle17"></div>
+        <div className="rectangle18"></div>
+        <div className="rectangle19"></div>
+        <div className="rectangle20"></div>
+        <div className="rectangle21"></div>
+        <div className="rectangle22"></div>
+        <div className="rectangle23"></div>
+        <div className="rectangle24"></div>
+        <div className="rectangle25"></div>
+        <div className="rectangle26"></div>
+       
+       
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+      </div>
     </div>
   );
 };
