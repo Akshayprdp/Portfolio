23e13fdb42e6faa1b2aa86cc530919ad823a244c
Akshay P <akshayprdp21@gmail.com>
2024-09-24 12:59:24 +0530

    socialmedia

--- src/Links/Links.jsx
+++ src/Links/Links.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
-import { FaGithub, FaInstagram, FaLinkedin,} from 'react-icons/fa';
-import './Links.css'
+import React from 'react';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import './Links.css';
 
 function Links() {
-  const links = [
-    { href: 'https://github.com/Akshayprdp', icon: <FaGithub className="link-icon" />, label: 'GitHub' },
-    { href: 'https://www.instagram.com/akshay_prdp/', icon: <FaInstagram className="link-icon" />, label: 'Instagram' },
-    { href: 'https://www.linkedin.com/in/akshayprdp21/', icon: <FaLinkedin className="link-icon" />, label: 'LinkedIn' },
-  ];
-
   return (
     <div className='link-container' id='Links'>
       <div className='about-work'>
-      <h1 className='Aboutlink'>On the web -</h1>
-      <h1 className='More-work'>More of my work</h1>
+        <h1 className='Aboutlink'>On the web -</h1>
+        <h1 className='More-work'>More of my work</h1>
       </div>
       <div className="links">
-        {links.map((link, index) => (
-          <a
-            key={index}
-            href={link.href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            {link.icon}
-            <span>{link.label}</span>
-          </a>
-        ))}
-      </div>
+        <a
+          href="https://github.com/Akshayprdp"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link1"
+        >
+          <FaGithub className="link-icon" />
+          <span>GitHub</span>
+        </a>
 
+        <a
+          href="https://www.instagram.com/akshay_prdp/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link2"
+        >
+          <FaInstagram className="link-icon" />
+          <span>Instagram</span>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/akshayprdp21/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link3"
+        >
+          <FaLinkedin className="link-icon" />
+          <span>LinkedIn</span>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Links
+export default Links;
